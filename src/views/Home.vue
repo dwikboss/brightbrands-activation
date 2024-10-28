28ed49dd62004618f117c98f4b63e402d0a293ca
dwikboss <dwikboss99@gmail.com>
2024-10-28 13:29:56 +0100

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="page home">
+    <div class="full-width">
+      <h1>BrightBrands activation</h1>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HomeView',
+});
+</script>
+
+<style lang="scss" scoped>
+.page.home {
+  background-color: var(--black);
+  height: 100vh;
+
+  .full-width {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+
+    .logo {
+      max-width: 200px;
+    }
+  }
+
+  h1 {
+    margin-top: 32px;
+    margin-bottom: 16px;
+    color: var(--white);
+    text-align: center;
+  }
+
+  h2 {
+    color: var(--white);
+    font-weight: 200;
+  }
+}
+</style>
