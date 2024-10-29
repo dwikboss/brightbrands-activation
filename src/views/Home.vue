bc249f1df4f38f507594952df76f5d8b1f81f512
dwikboss <dwikboss99@gmail.com>
2024-10-29 10:00:36 +0100

    feat: added input control

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,43 @@
   <div class="page home">
     <div class="full-width">
       <h1>BrightBrands activation</h1>
+      <div class="input-area">
+        <div class="full-width">
+          <input ref="inputField" v-model="input" placeholder="Website hier" type="text" @keyup.enter="makeRequest" />
+          <button @click="makeRequest">send</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import axios from 'axios';
 
 export default defineComponent({
   name: 'HomeView',
+  data() {
+    return {
+      input: '' as string,
+      response: '' as string,
+    };
+  },
+  methods: {
+    async makeRequest() {
+      console.log(this.input);
+      try {
+        const response = await axios.post('http://localhost:3000/message', {
+          inputMessage: this.input,
+        });
+        this.response = JSON.parse(response.data.reply.choices[0].message.content);
+        console.log(this.response);
+      } catch (error) {
+        console.error('Error sending chat:', error);
+        console.error('Max retries reached. Giving up.');
+      }
+    },
+  },
 });
 </script>
 
